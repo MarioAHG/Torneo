8aa658e010e08a3beaa628f46e90c579a6acc268
Technology Rooms <technologyrooms@colsanjose.edu.co>
2024-11-20 12:39:30 -0500

    3

--- Main.java
+++ Main.java
@@ -5,41 +5,46 @@ import java.util.Scanner;
 import java.io.BufferedReader;
 
 class Main {
+    static String[] options = new String[] {"Canchas disponibles", "Hora de inicio", "Dias de duración"};
     public static void Condiciones(String[] args) {
-        String[] cond = new String[5];
-        Scanner rr = new Scanner(System.in);
-        for (int i = 0; i < cond.length - 1; i++) {
+        String[] cond = new String[3];
+        System.out.println("Ponga las siguientes condiciones:");
+        for (int i = 0; i < options.length; i++) {
+            System.out.println(options[i]);
+            Scanner rr = new Scanner(System.in);
             String ss = rr.nextLine();
             cond[i] = ss;
-        }
 
+        }
     }
 
     public static void main(String[] args) {
-        jugador[] jugadores = new jugador[4];
-        Scanner sc = new Scanner(System.in);
-        String option = sc.nextLine();
-        switch (option) {
-            case "1":
-                //ArrayList<jugador> A = new ArrayList<>();
-                //A.add();
-                break;
-            case "2":
+        Condiciones(args);
+        // jugador[] jugadores = new jugador[4];
+        // Scanner sc = new Scanner(System.in);
+        // String option = sc.nextLine();
+        // switch (option) {
+        //     case "1":
+        //         //ArrayList<jugador> A = new ArrayList<>();
+        
+        //         //A.add();
+        //         break;
+        //     case "2":
 
-                break;
-            case "3":
+        //         break;
+        //     case "3":
 
-                break;
-            default:
-                System.out.println("Error");
-                break;
-        }
+        //         break;
+        //     default:
+        //         System.out.println("Error");
+        //         break;
+        // }
     }
 
     public static jugador inputJ(){
         jugador jSubmit = new jugador();
-        System.out.println("Ingrese el nombre del jugador" + ;);
-        jugadores[0] = jSubmit;
+        System.out.println("Ingrese el nombre del jugador");
+        //jugadores[0] = jSubmit;
         return jSubmit;
     }
 
